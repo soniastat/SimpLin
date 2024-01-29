3cb9af1996e9c1cfcc105292e894242accbe3607
soniastat <soniakhatun.du.29@gmail.com>
2024-01-28 20:20:45 -0700

    Write description, change the y in test file, add checking error if not numeric x and y

--- R/SimpLinR.R
+++ R/SimpLinR.R
@@ -5,6 +5,9 @@ SimpLinR <- function(x, y){
     stop("Input vectors must have the same length.")
   }
   
+  if (!is.numeric(x) | !is.numeric(y)) {
+    print("Either x or y (or both) is not a numeric vector")
+  }
   
   ## If there is no error, run SimpLinCpp
   result <- SimpLinCpp(x, y)
